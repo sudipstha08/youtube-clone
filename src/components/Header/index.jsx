23297e0f5a013c90749ef40c3ffe3a15ed91631c
Sudip Shrestha <sudipstha08@gmail.com>
2021-07-30 18:17:10 +0545

    fix: header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react'
 import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import Popover from '@material-ui/core/Popover'
 import NotificationsIcon from '@material-ui/icons/Notifications'
 import VideoCallIcon from '@material-ui/icons/VideoCall'
 import SearchIcon from '@material-ui/icons/Search'
 import AppsIcon from '@material-ui/icons/Apps'
 import MenuIcon from '@material-ui/icons/Menu'
 import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
 import { GlobalContext } from '../../context/GlobalState'
 import YoutubeLogo from '../../assets/icons/youtube.png'
 import ProfileImage from '../../assets/images/coder.jpg'
@@ -23,6 +25,19 @@ const Header = ({ history }) => {
       history.push(`/results?search_query=${searchQuery}`)
     }
   }
+
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const open = Boolean(anchorEl)
+  const id = open ? 'simple-popover' : undefined
 
   return (
     <header className="header">
@@ -57,7 +72,28 @@ const Header = ({ history }) => {
         <VideoCallIcon className="header__icon" />
         <AppsIcon className="header__icon" />
         <NotificationsIcon className="header__icon" />
-        <Avatar alt="image" src={ProfileImage} />
+
+        <Avatar alt="image" src={ProfileImage} onClick={handleClick} />
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          className="header__avatar"
+          style={{ display: 'flex', flexDirection: 'column' }}
+        >
+          <Button variant="contained" color="primary">
+            Sign out
+          </Button>
+        </Popover>
       </div>
     </header>
   )
